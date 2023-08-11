9c30d6cad20f4a826a9230485a5691bd477f2a32
prinnyprin <milishania@gmail.com>
2023-08-11 18:02:06 -0400

    Update UI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,65 @@
 import './style.css';
-import { Timer } from './timer';
-console.log("Set up");
-var test = Timer(5);
-test.startTimer();
+import { ELEM, Clock, Buttons } from './ui';
+
+/*SETUP*/
+let main = ELEM.get("main");
+ELEM.add(main, [Clock(), Buttons()]);
+
+/*TIMER*/
+export const Timer = (timeTotal, ui, secs) => {
+    var timeElapsed = 0;
+    var actualTime = timeTotal / 60;
+    console.log(actualTime);
+    var timer;
+    const startTimer= () =>{
+        timer = setInterval(()=>{
+            if(timeElapsed === timeTotal-1){
+                stopTimer();
+            }
+            ++timeElapsed;
+            setUI();
+        }, 1000);
+    }
+    const stopTimer = () => {
+        clearInterval(timer);
+    }
+    const setUI = () => {
+        if(timeElapsed < 60){ui.innerHTML = actualTime; secs.innerHTML = timeElapsed;}
+        else{
+            let val = actualTime - Math.floor(timeElapsed/60);
+            secs.innerHTML = timeElapsed - (Math.floor(timeElapsed/60)*60);
+            (val < 10) ? ui.innerHTML = "0" + val : ui.innerHTML = val;
+        }
+    }
+    return {timeTotal, startTimer, stopTimer};
+}
+
+/*EVENTS*/
+let pomoBtn = ELEM.get("#pomo-btn");
+let timeUI = ELEM.get("#time");
+let secsUI = ELEM.get("#secs");
+let timer = Timer(20*60, timeUI, secsUI);
+let pomo = false;
+let brek = false;
+
+function setPomo(){
+    if(!pomo){
+        pomoBtn.innerHTML = "STOP";
+        timeUI.innerHTML = "20";
+        secsUI.innerHTML = "0";
+        timer.startTimer();
+        pomo = true;
+    }
+    else{
+        pomoBtn.innerHTML = "POMO";
+        timer.stopTimer();
+        pomo = false;
+    }
+}
+
+pomoBtn.addEventListener("click", ()=>{
+    setPomo();
+});
+
+
+
---
+++ src/ui.js
@@ -0,0 +1,40 @@
+//UTILITIES
+export const ELEM = (()=>{
+    const create = (type, vals, selector, attr) =>{ //CREATE
+        let elem = document.createElement(type);
+        if(vals != null){(typeof vals == "string") ? (elem.innerHTML = vals) : add(elem, vals);}
+        if(selector != null){(typeof selector == "string") ? (elem.id = selector) : classIn(elem, selector);}
+        if(attr != null){attrIn(elem, attr)}
+        return elem;
+    }
+    const get = (selector) =>{//RETRIEVE
+        return document.querySelector(selector);
+    }
+    const getAll = (selector) =>{//RETRIEVE
+        return document.querySelectorAll(selector);
+    }
+    const add = (elem, children) =>{//UPDATE
+        for(let i = 0; i < children.length; i++){
+            elem.appendChild(children[i]);
+        }
+    }
+    const classIn = (elem, classes) =>{//UPDATE
+        for(let i = 0; i < classes.length; i++){
+            elem.classList.add(classes[i]);
+        }
+    }
+    const attrIn = (elem, attr) => {
+        for(let i = 0; i < attr.length; i+=2){
+            elem.setAttribute(attr[i], attr[i+1]);
+        }
+    }
+    return {create, get, getAll, add, attrIn};
+})();
+
+export function Clock(){
+    return ELEM.create("div", [ELEM.create("div", "00", "time"), ELEM.create("div", "0", "secs")], "clock");
+}
+
+export function Buttons(){
+    return ELEM.create("div", [ELEM.create("button", "POMO", "pomo-btn"), ELEM.create("button", "BREAK", "break-btn")], "buttons");
+}
